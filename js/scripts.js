767c3b36aab2af7cfa9fb174d1b5b5b9504f8f51
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-04-06 18:00:04 -0300

    Data Footer dinamico

--- js/scripts.js
+++ js/scripts.js
@@ -106,8 +106,8 @@ document.getElementById("service-next").onclick = function() {
 
 // Data Footer
 
+var ano_atual = new Date;
+ano_atual = ano_atual.getFullYear();
+document.getElementById("current_year").innerHTML = ano_atual;
 
 
-
-
-   
